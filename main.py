05c45c801774af9eb46ae635e19a1149f94fb138
PuuperttiRuma <tommi.a.haapanen@gmail.com>
2025-09-23 22:08:40 +0300

    Bookbot works by args

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_word_count, get_character_counts, sort_char_counts
 
 
@@ -7,7 +8,6 @@ def get_book_text(path):
 
 
 def main():
-    path = "books/frankenstein.txt"
     text = get_book_text(path)
     word_count = get_word_count(text)
     character_counts = get_character_counts(text)
@@ -24,4 +24,8 @@ def main():
     print("============= END ===============")
 
 
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+path = sys.argv[1]
 main()
